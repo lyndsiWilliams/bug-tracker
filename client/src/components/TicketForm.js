2fb8e33308c432ef76c089c168692b0c141974e3
lyndsiWilliams <kcatgirl@gmail.com>
2020-11-18 09:51:26 -0600

    Basic form UI setup complete

---
+++ client/src/components/TicketForm.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const TicketForm = () => {
+  const [ticket, setTicket] = useState({
+    title: '',
+    description: ''
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("handleSubmit", ticket);
+  };
+
+  const handleChange = event => {
+    setTicket({ ...ticket, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        onChange={handleChange}
+        value={ticket.title}
+      />
+      <label>Description</label>
+      <input
+        type="text"
+        name="description"
+        placeholder="Description"
+        onChange={handleChange}
+        value={ticket.description}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default TicketForm;
